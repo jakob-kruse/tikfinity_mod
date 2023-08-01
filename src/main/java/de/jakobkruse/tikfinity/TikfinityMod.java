4c53dac2b426db1404456a245685cf9056aae3f9
Jakob Kruse <contact@jakob-kruse.de>
2023-08-02 01:57:23 +0200

    JETZT ALTER

---
+++ src/main/java/de/jakobkruse/tikfinity/TikfinityMod.java
@@ -0,0 +1,15 @@
+package de.jakobkruse.tikfinity;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TikfinityMod implements ModInitializer {
+    public static final String MOD_ID = "tikfinity";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+
+    @Override
+    public void onInitialize() {
+    }
+}
