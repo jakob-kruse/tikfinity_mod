3a220613e90ff73a755ec452f68344640c148486
Jakob Kruse <contact@jakob-kruse.de>
2023-08-02 01:36:16 +0200

    Fix Preflight and CORS Add /v1/server endpoint

--- src/main/java/de/jakobkruse/tikfinity/CommandServer.java
+++ src/main/java/de/jakobkruse/tikfinity/CommandServer.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 
 public class CommandServer {
@@ -27,7 +28,8 @@ public class CommandServer {
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-            server.createContext("/v1/exec", new HTTPServer());
+            server.createContext("/v1/server", new HTTPServer());
+            server.createContext("/v1/server/exec", new HTTPServer());
             server.setExecutor(null);
         }
         server.start();
@@ -47,17 +49,84 @@ public class CommandServer {
     private class HTTPServer implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
+            t.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+            t.getResponseHeaders().add("Access-Control-Allow-Methods", "*");
+            t.getResponseHeaders().add("Access-Control-Allow-Headers", "*");
+
+            if (t.getRequestMethod().equalsIgnoreCase("OPTIONS")) {
+                t.sendResponseHeaders(204, -1);
+                return;
+            }
+
+            switch (t.getRequestURI().toString()) {
+                case "/v1/server" -> handleServer(t);
+                case "/v1/server/exec" -> handleExecute(t);
+                default -> t.sendResponseHeaders(404, 0);
+            }
+
+            t.close();
+
+        }
+
+        private void handleExecute(HttpExchange t) throws IOException {
             InputStream reqStream = t.getRequestBody();
 
             String requestString = IOUtils.toString(reqStream, StandardCharsets.UTF_8);
 
-            String command = requestString.split("=")[1];
+            String command = requestString.contains("command=") ? URLDecoder.decode(requestString.split("=")[1], StandardCharsets.UTF_8)  : "";
 
             TikfinityMod.LOGGER.info("Received command: " + command);
 
             executor.execute(command);
 
             String response = "OK";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+
+        private void handleServer(HttpExchange t) throws IOException {
+            t.getResponseHeaders().add("Content-Type", "application/json");
+            String response = """
+                    {
+                      "name": "TikFinity Mod",
+                      "motd": "string",
+                      "version": "1.20.1",
+                      "bukkitVersion": "string",
+                      "tps": "string",
+                      "health": {
+                        "cpus": 0,
+                        "uptime": 0,
+                        "totalMemory": 0,
+                        "maxMemory": 0,
+                        "freeMemory": 0
+                      },
+                      "bannedIps": [
+                        {
+                          "target": "string",
+                          "source": "string",
+                          "reason": "string",
+                          "expiration": "2023-08-01T22:55:55.756Z"
+                        }
+                      ],
+                      "bannedPlayers": [
+                        {
+                          "target": "string",
+                          "source": "string",
+                          "reason": "string",
+                          "expiration": "2023-08-01T22:55:55.756Z"
+                        }
+                      ],
+                      "whitelistedPlayers": [
+                        {
+                          "uuid": "string",
+                          "name": "string"
+                        }
+                      ],
+                      "maxPlayers": 0,
+                      "onlinePlayers": 0
+                    }""";
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
